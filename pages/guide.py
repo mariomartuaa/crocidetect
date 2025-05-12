b2a2a1ff79c1eb4a8f8990de1e7435b35ce91a72
mariomartuaa <134026274+mariomartuaa@users.noreply.github.com>
2025-05-12 09:31:08 +0700

    Update guide.py

--- pages/guide.py
+++ pages/guide.py
@@ -20,7 +20,7 @@ st.markdown("""
 
 [data-testid="stAppViewBlockContainer"] {
     background: linear-gradient(45deg, #d7eac2 25%, #a9d7a9 50%, #f0e58a 75%, #e3d26f 100%);
-    animation: backgroundGradientShift 10s ease infinite;
+    animation: backgroundGradientShift 20s ease infinite;
     background-size: 300% 300%;
     background-attachment: fixed;
 }
@@ -166,20 +166,20 @@ with margin_col1:
     st.write("")
     
 with margin_col2:
-    st.header("Langkah-langkah Penggunaan", divider="green")
+    st.header("Cara Penggunaan Aplikasi", divider="green")
     st.subheader("1. Upload Gambar")
     st.write("Unggah gambar larva *Crocidolomia pavonana* dalam format `.jpg`, `.jpeg`, atau `.png`.")
-    with st.expander("Baca selengkapnya"):
+    with st.expander("Lihat selengkapnya"):
         st.image("assets/guide/1.png")
 
     st.subheader("2. Lihat Pratinjau Gambar")
     st.write("Gambar yang Anda unggah akan otomatis ditampilkan sebagai pratinjau di halaman utama.")
-    with st.expander("Baca selengkapnya"):
+    with st.expander("Lihat selengkapnya"):
         st.image("assets/guide/2.png")
 
     st.subheader("3. Klik Tombol 'Klasifikasi Gambar'")
     st.write("Tekan tombol ini untuk memulai proses klasifikasi tahapan instar menggunakan model deep learning.")
-    with st.expander("Baca selengkapnya"):
+    with st.expander("Lihat selengkapnya"):
         st.image("assets/guide/3.png")
 
     # st.subheader("4. Tunggu Proses Prediksi")
@@ -187,17 +187,17 @@ with margin_col2:
 
     st.subheader("4. Tinjau Hasil Klasifikasi")
     st.write("Hasil klasifikasi ditampilkan dalam bentuk kelas instar, nilai akurasi, dan tabel confidence untuk semua kelas.")
-    with st.expander("Baca selengkapnya"):
+    with st.expander("Lihat selengkapnya"):
         st.image("assets/guide/5.png")
 
     st.subheader("5. Lihat Visualisasi Grad-CAM")
     st.write("Grad-CAM menunjukkan area penting dari gambar yang memengaruhi keputusan model. Ini membantu memahami hasil prediksi.")
-    with st.expander("Baca selengkapnya"):
+    with st.expander("Lihat selengkapnya"):
         st.image("assets/guide/6.png")
         
     st.subheader("6. Lihat Riwayat (Opsional)")
     st.write("Buka menu **History** di sidebar untuk melihat gambar dan visualisasi Grad-CAM sebelumnya selama sesi berjalan.")
-    with st.expander("Baca selengkapnya"):
+    with st.expander("Lihat selengkapnya"):
         st.image("assets/guide/7.png")
 
 with margin_col3:
