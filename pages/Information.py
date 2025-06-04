760ac0856887d8eb508bfcf59c74d606f7d8d232
mariomartuaa <134026274+mariomartuaa@users.noreply.github.com>
2025-06-04 21:17:16 +0700

    Update Information.py

--- pages/Information.py
+++ pages/Information.py
@@ -36,7 +36,7 @@ with col2:
         """,
         unsafe_allow_html=True)
     
-st.markdown("""<hr style="background-color: black; margin-left:5rem; margin-right:5rem;">""",unsafe_allow_html=True)
+st.markdown("""<hr style="background-color: black;">""",unsafe_allow_html=True)
 st.markdown(f'<h1 style="text-align: center; font-size: 30px; color: #2e5339;">Tanaman Inang</h1>', unsafe_allow_html=True)
 
 col1, col2, col3, col4 = st.columns(4)
@@ -57,7 +57,7 @@ with col4:
     st.markdown(f'<h1 style="text-align: center; font-size: 20px; color: #2e5339;">Lobak</h1>', unsafe_allow_html=True)
     st.image("assets/lobak.jpg", use_column_width=True)
 
-st.markdown("""<hr style="background-color: black; margin-left:5rem; margin-right:5rem;">""",unsafe_allow_html=True)
+st.markdown("""<hr style="background-color: black;">""",unsafe_allow_html=True)
 
 
 st.markdown(f'<h1 style="text-align: center; font-size: 40px; color: #2e5339;">Tahapan Instar Larva</h1>', unsafe_allow_html=True)
@@ -87,7 +87,7 @@ with cols[3]:
 
 st.markdown("""<hr style="background-color: black;">""",unsafe_allow_html=True)
 st.markdown("""
-        <div style="padding-left: 5rem; padding-right: 5rem; padding-bottom: 5rem; font-size: clamp(0.9rem, 1.5vw, 1.2rem);">
+        <div style="padding-bottom: 5rem; font-size: clamp(0.9rem, 1.5vw, 1.2rem);">
                 Penjelasan Berdasarkan Penelitian:<br>
                 📚 Rany Badjo et al. (2015). SERANGAN HAMA ULAT KROP (Crocidolomia pavonana F.) PADA TANAMAN KUBIS (Brassica oleracea var. capitata L.) DI KELURAHAN KAKASKASEN II, KECAMATAN TOMOHON UTARA, KOTA TOMOHON. Vol. 6 No. 14 (2015). DOI: https://doi.org/10.35791/cocos.v6i14.8755<br>
                 📚 Frangky J. Paat dan Jantje Pelealu. (2021). MORFOLOGI DAN PERILAKU HAMA Crocidolomia pavonana PADA TANAMAN KUBIS. Vol. 12 No. 4 (2020): EDISI OKTOBER-DESEMBER 2020. DOI: https://doi.org/10.35791/cocos.v1i1.31819<br>
