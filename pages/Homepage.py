2ee8d0d0be9e852ca8a931cca3326b3bca077a91
mariomartuaa <134026274+mariomartuaa@users.noreply.github.com>
2025-06-04 20:57:15 +0700

    Update Homepage.py

--- pages/Homepage.py
+++ pages/Homepage.py
@@ -331,7 +331,7 @@ st.markdown("""<hr style="background-color: black; margin-left:5rem; margin-righ
 
 
 st.markdown(f'<h1 style="text-align: center; font-size: 40px; color: #2e5339;">Tahapan Instar Larva</h1>', unsafe_allow_html=True)
-st.markdown(f'<h1 style="text-align: center; font-size: 20px; color: #2e5339; margin-left:5rem; margin-right:5rem;">Sebuah instar adalah sebuah tahap perkembangan pada artropoda seperti serangga. Perbedaan diantara instar umumnya dapat dilihat dari perubahan ukuran tubuh, warna, pola, jumlah segmen tubuh, dan lebar kepala.</h1>', unsafe_allow_html=True)
+st.markdown(f'<h1 style="font-size: 20px; color: #2e5339; margin-left:5rem; margin-right:5rem;">Sebuah instar adalah sebuah tahap perkembangan pada artropoda seperti serangga. Perbedaan diantara instar umumnya dapat dilihat dari perubahan ukuran tubuh, warna, pola, jumlah segmen tubuh, dan lebar kepala.</h1>', unsafe_allow_html=True)
 
 instar_data = [
     {
@@ -359,7 +359,7 @@ instar_data = [
 cols = st.columns(4)
 for i in range(4):
     with cols[i]:
-        st.markdown(f'<h1 style="font-size: 20px; color: #2e5339;">{instar_data[i]["title"]}</h1>', unsafe_allow_html=True)
+        st.markdown(f'<h1 style="text-align: center; font-size: 20px; color: #2e5339;">{instar_data[i]["title"]}</h1>', unsafe_allow_html=True)
         st.image(instar_data[i]["img"], use_column_width=True)
         st.markdown(f'<div class="card">{instar_data[i]["desc"]}</div>', unsafe_allow_html=True)
 
