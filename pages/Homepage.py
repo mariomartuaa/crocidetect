7bcd87e751b061f1e9f98f87ca62a79866d7ea31
mariomartuaa <134026274+mariomartuaa@users.noreply.github.com>
2025-06-04 23:24:40 +0700

    Update Homepage.py

--- pages/Homepage.py
+++ pages/Homepage.py
@@ -329,12 +329,12 @@ with col4:
 
 st.markdown("""<hr style="background-color: black; margin-left:5rem; margin-right:5rem;">""",unsafe_allow_html=True)
 
-st.markdown(f'<h1 style="text-align: center; font-size: 40px; color: #2e5339;">Pengendalian</h1>', unsafe_allow_html=True)
+st.markdown(f'<h1 style="text-align: center; font-size: 30px; color: #2e5339;">Pengendalian Hama</h1>', unsafe_allow_html=True)
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.markdown(f'<h1 style="text-align: center; font-size: 30px; color: #2e5339;">Pestisida Nabati Tanaman Mindi</h1>', unsafe_allow_html=True)
+    st.markdown(f'<h1 style="text-align: center; font-size: 20px; color: #2e5339;">Pestisida Nabati Tanaman Mindi</h1>', unsafe_allow_html=True)
     img1_col = st.columns([1,3,1])
     with img1_col[0]:
         st.write("")
@@ -369,7 +369,7 @@ with col1:
             unsafe_allow_html=True)
 
 with col2:
-    st.markdown(f'<h1 style="text-align: center; font-size: 30px; color: #2e5339;">Pestisida Nabati Tanaman Pacar cina</h1>', unsafe_allow_html=True)
+    st.markdown(f'<h1 style="text-align: center; font-size: 20px; color: #2e5339;">Pestisida Nabati Tanaman Pacar cina</h1>', unsafe_allow_html=True)
     img2_col = st.columns([1,3,1])
     with img2_col[0]:
         st.write("")
