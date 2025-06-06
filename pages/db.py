54bfc69389c5d23648317a6f00adc91d0eec1faf
mariomartuaa <134026274+mariomartuaa@users.noreply.github.com>
2025-06-06 20:06:45 +0700

    Update db.py

--- pages/db.py
+++ pages/db.py
@@ -1,20 +1,5 @@
 import sqlite3
 import os
-from streamlit_cookies_manager import EncryptedCookieManager
-import streamlit as st
-
-cookies = EncryptedCookieManager(
-prefix="crocidetect_",
-password=st.secrets["COOKIE_SECRET"])
-
-if not cookies.ready():
-    st.stop()
-
-user_id = cookies.get("user_id")
-if user_id is None:
-    user_id = str(uuid.uuid4())
-    cookies["user_id"] = user_id
-    cookies.save()
 
 DB_PATH = "data/predictions.db"
 
