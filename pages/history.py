ea611bc457cb284bf57f2d5914bb76b9dad0b3c4
mariomartuaa <134026274+mariomartuaa@users.noreply.github.com>
2025-06-09 12:53:53 +0700

    Update history.py

--- pages/history.py
+++ pages/history.py
@@ -2,19 +2,16 @@ import streamlit as st
 from streamlit_cookies_manager import EncryptedCookieManager
 from pages.db import get_predictions_by_user, delete_prediction
 import pandas as pd
-from PIL import Image
-import io
+import json
+from datetime import datetime
 
-cookies = EncryptedCookieManager(
-    prefix="crocidetect_",
-    password=st.secrets["COOKIE_SECRET"])
+cookies = EncryptedCookieManager(prefix="crocidetect_", password=st.secrets["COOKIE_SECRET"])
 if not cookies.ready():
     st.stop()
 
 user_id = cookies["user_id"]
 
 margin_col1, margin_col2, margin_col3 = st.columns([1, 3, 1])
-
 with margin_col1:
     st.write("")
 
@@ -27,18 +24,25 @@ with margin_col2:
         st.info("Belum ada riwayat prediksi.")
     else:
         for rec in records:
-            rec_id, timestamp, orig_img_blob, gradcam_img_blob, pred_class, conf_json = rec
+            rec_id = rec['id']
+            timestamp = rec['timestamp']
+            orig_img_url = rec['original_image']
+            gradcam_img_url = rec['gradcam_image']
+            pred_class = rec['predicted_class']
+            conf_json = json.loads(rec['confidence_table'])
 
-            st.write(f"### Prediksi pada: {timestamp}")
 
-            # Gambar dalam 2 kolom
+            dt = datetime.fromisoformat(timestamp)
+            # Format ulang ke string tanpa 'T'
+            formatted = dt.strftime("%d-%m-%Y Pukul %H:%M:%S")
+            st.write(f"### Prediksi pada: {formatted}")
+
             img_col1, img_col2 = st.columns(2)
             with img_col1:
-                st.image(Image.open(io.BytesIO(orig_img_blob)), caption="Gambar Asli", use_column_width=True)
+                st.image(orig_img_url, caption="Gambar Asli", use_column_width=True)
             with img_col2:
-                st.image(Image.open(io.BytesIO(gradcam_img_blob)), caption="Grad-CAM Visualisasi", use_column_width=True)
+                st.image(gradcam_img_url, caption="Grad-CAM", use_column_width=True)
 
-            # Hasil prediksi dan tabel confidence dalam 2 kolom
             hasil_col1, hasil_col2 = st.columns(2)
             with hasil_col1:
                 st.markdown(f"""
@@ -49,12 +53,12 @@ with margin_col2:
                 """, unsafe_allow_html=True)
 
             with hasil_col2:
-                df_conf = pd.read_json(io.StringIO(conf_json))
+                df_conf = pd.DataFrame(conf_json)
                 st.dataframe(df_conf.style.format({'Akurasi (%)': '{:.2f}'}))
 
             if st.button("Hapus", key=f"del_{rec_id}"):
-                    delete_prediction(rec_id)
-                    st.rerun()
+                delete_prediction(rec_id)
+                st.rerun()
 
             st.divider()
 
